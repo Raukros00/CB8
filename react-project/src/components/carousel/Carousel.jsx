f87fa905325b798fa5c812fa2dffd67edb3b3ce9
Alessandro Dominici <57728146+Raukros00@users.noreply.github.com>
2024-02-11 19:16:42 +0100

    Delete react-project directory

--- react-project/src/components/carousel/Carousel.jsx
+++
@@ -1,8 +0,0 @@
-import Pokemon from "../pokemon";
-import styles from "./index.module.scss";
-
-const Carousel = ({ children }) => {
-  return <div className={styles.PokemonCarousel}>{children}</div>;
-};
-
-export default Carousel;
