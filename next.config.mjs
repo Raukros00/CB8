ac2acb8cff41044321046bcaf20e6ae0b35888b6
Alessandro Dominici <57728146+Raukros00@users.noreply.github.com>
2024-02-11 18:40:50 +0100

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com", "raw.githubusercontent.com"],
-  },
-};
-
-export default nextConfig;
